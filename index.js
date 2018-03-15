225ecdfb84d03e8910a8f3cc9d18b155fa9806d2
Kriselle Ramac <krisellaella@gmail.com>
2018-03-15 17:52:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
 
-function HolidayTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!"
 }
 
